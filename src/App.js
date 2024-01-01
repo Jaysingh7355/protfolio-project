dc239939cb08d015390f937b3a8869a5b7897158
Jay singh <jaysingh7355@gmail.com>
2024-01-01 22:55:26 +0530

    protfolio websit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+//import About from "./components/About";
+// import Contact from "./components/contact";
+//import Navbar from "./components/navbar";
+// import Projects from "./components/projects";
+ import Skills from "./components/Skills";
+//import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      
+      <Skills />
+    </main>
+  );
+}
+
+/* 
+my-portfolio
+├── README.md
+├── node_modules
+├── package.json
+├── .gitignore
+├── public
+│   ├── favicon.ico
+│   ├── index.html
+│   └── manifest.json
+└── src
+    ├── App.js
+    ├── data.js
+    ├── index.css
+    ├── index.js
+    └── components
+        ├── About.js
+        ├── Contact.js
+        ├── Navbar.js
+        ├── Projects.js
+        ├── Skills.js
+        └── Testimonials.js
+
+*/
